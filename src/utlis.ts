86815b449b2cf133426f57d8443e72a107906a2e
soiloui <soilouiem@gmail.com>
2021-03-25 01:00:12 +0100

    default client time zone on table

--- src/utlis.ts
+++ src/utlis.ts
@@ -6,4 +6,15 @@ export const getCalendarDate = (date: Date, days: number): Date => {
 }
 
 // returns compact string of provided date in YYYY-MM-DD format
-export const getCompactDate = (date: Date): string => date.toISOString().split('T')[0];
+export const getCompactDate = (date: Date): string => {
+  const year = date.getFullYear();
+  let month = '' + (date.getMonth() + 1);
+  let day = '' + date.getDate();
+
+  if (month.length < 2)
+    month = '0' + month;
+  if (day.length < 2)
+    day = '0' + day;
+
+  return [year, month, day].join('-');
+}
